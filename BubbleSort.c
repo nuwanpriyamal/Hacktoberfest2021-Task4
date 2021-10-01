722657dbd1bc9164f73ae4f7d7dc137905c0ce1b
nuwanpriyamal <42827296+nuwanpriyamal@users.noreply.github.com>
2021-10-01 23:02:16 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -4,19 +4,19 @@
 int main()
 {
     bool swap;
-    int n=5,j;
-    int arr[5]={58,89,71,35,6};
+    int n=5,x;
+    int arr[5]={68,99,81,45,16};
     
     
     do{
         swap=false;
-        for(j=0;j<n;j++)
+        for(x=0;x<n;x++)
         {
-            if (arr[j]>arr[j-1]);
+            if (arr[x]>arr[x-1]);
             {
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp
+                temp=arr[x];
+                arr[x]=arr[x+1];
+                arr[x+1]=temp
                 swap=false;
             }
         }
